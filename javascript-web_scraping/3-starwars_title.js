8e3d0dbd2d1c16393297e5c0d9899a252e2764d8
FacundoV21 <6756@holbertonstudents.com>
2024-01-26 13:41:49 -0800

    fixed task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request.get(`https://swapi-api.hbtn.io/api/films/:id/${process.argv[2]}`, (err, response, body) => {
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
   if (err) throw err;
   console.log(JSON.parse(body).title);
 });
