51ca98b829499e8b548bfefd1a3db65293e9762d
FacundoV21 <6756@holbertonstudents.com>
2024-01-26 15:01:03 -0800

    added task 4 and 5

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], (err, response, body) => {
+  if (err) throw err;
+  const elems = JSON.parse(body);
+  let count = 0;
+
+  for (const i of elems.results){
+    for (const j of i.characters){
+      if (j.includes('18')){
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
