6d6205b73013b142ed47cfed0766cdc773d27368
FacundoV21 <6756@holbertonstudents.com>
2024-02-04 16:37:44 +0000

    added tasks 0 to 2 and README.md

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+export default function countStudents(path) {
+  try {
+    const data = (fs.readFileSync(path, 'utf-8'));
+    const dataSep = data.split('\n');
+    const retArr = dataSep.filter((x) => x.length > 0);
+    console.log(`Number of students: ${retArr.length - 1}`);
+
+    let f1 = 0;
+    let f2 = 0;
+    const arr1 = [];
+    const arr2 = [];
+    for (const line of retArr) {
+      const data = line.split(',');
+      if (data[3] === 'CS') {
+        arr1.push(data[0]);
+        f1 += 1;
+      }
+      if (data[3] === 'SWE') {
+        arr2.push(data[0]);
+        f2 += 1;
+      }
+    }
+    console.log(`Number of students in CS: ${f1}. List: ${arr1.join(', ')}`);
+    console.log(`Number of students in SWE: ${f2}. List: ${arr2.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
